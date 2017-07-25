02ba818b0023171a14fa82dc242d2b4e82b674bf
Marginean Madalina <margineanmadalina@Margineans-MacBook-Pro.local>
2017-07-25 12:52:20 +0300

    Calculator finished

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -1,52 +1,59 @@
 // calculator logic
-var numericButtonsContainer = document.getElementById("numeric-buttons");
 
-for(var i=0;i<3;i++)
-{
-  var line = document.createElement("div");
-  line.id = "line"+(i+1);
-  for(var j=1;j<=3;j++){
+createNumberButtons();
+createFunctionButtons();
+
+function createNumberButtons(){
+  calculator.numericButtonsContainer = document.getElementById("numeric-buttons");
+  calculator.numericButton = [];
+  for(var i=0;i<3;i++)
+  {
+    var line = document.createElement("div");
+    line.id = "line"+(i+1);
+    applyStyleForLines(line);
+    for(var j=1;j<=3;j++){
       var button = document.createElement("button");
       var buttonText = document.createTextNode(i*3+j);
-      button.style.width = '40px';
-      button.style.height = '40px';
+      applyStyleForButtons(button);
       button.id = "button"+(i*3+j);
+      calculator.numericButton.push(button);
       button.appendChild(buttonText);
       line.appendChild(button);
     }
-    numericButtonsContainer.appendChild(line);
-}
-
-line1.style.position = 'relative';
-line1.style.top = '50px';
-line1.style.left = '45px';
-
-line2.style.position = 'relative';
-line2.style.top = '50px';
-line2.style.left = '45px';
-
-line3.style.position = 'relative';
-line3.style.top = '50px';
-line3.style.left = '45px';
-
-var functionButtonsContainer = document.getElementById("function-buttons");
-var functionButton = ['+', '-', '*', '/','='];
-for(var i=0;i<1;i++)
-{
-  var col = document.createElement("div");
-  col.id = "operators";
-  for(var j=0;j<=4;j++){
-    var button = document.createElement("button");
-    var buttonText = document.createTextNode(functionButton[j]);
-    button.style.width = '40px';
-    button.style.height = '40px';
-    button.id = functionButton[j];
-    button.appendChild(buttonText);
-    col.appendChild(button);
+    calculator.numericButtonsContainer.appendChild(line);
   }
-  functionButtonsContainer.appendChild(col);
 }
 
-operators.style.position = 'relative';
-operators.style.top = '10px';
-operators.style.left = '15px';
+function applyStyleForLines(line)
+{
+  line.style.position = 'relative';
+  line.style.top = '50px';
+  line.style.left = '45px';
+}
+
+function applyStyleForButtons(button)
+{
+  button.style.width = '40px';
+  button.style.height = '40px';
+}
+
+function createFunctionButtons(){
+  calculator.functionButtonsContainer = document.getElementById("function-buttons");
+  calculator.operators = ["+","-","*","/","="];
+  calculator.functionButton = [];
+  for(var i=0;i<1;i++)
+  {
+    var col = document.createElement("div");
+    col.id = "operators";
+    for(var j=0;j<=4;j++){
+      var button = document.createElement("button");
+      var buttonText = document.createTextNode(calculator.operators[j]);
+      applyStyleForButtons(button);
+      button.id = calculator.operators[j];
+      calculator.functionButton.push(button);
+      button.appendChild(buttonText);
+      col.appendChild(button);
+    }
+    calculator.functionButtonsContainer.appendChild(col);
+  }
+}
